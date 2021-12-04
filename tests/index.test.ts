26a4fa9a4aaa397b218a3a9df25411409df7d26f
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-12-04 16:25:46 +1100

    better testing

---
+++ tests/index.test.ts
@@ -0,0 +1,20 @@
+import { equal } from 'assert';
+import { request, SendTypes } from '../src/index';
+
+export async function Get() {
+	const res = await request('https://jsonplaceholder.typicode.com/todos/1').send()
+
+	equal(typeof res.json, 'object')
+}
+
+export async function Post() {
+	const res = await request('https://jsonplaceholder.typicode.com/posts', 'POST')
+		.body({
+			title: 'hello world!',
+			body: 'foobar',
+			userId: 101
+		}, SendTypes.JSON)
+		.send()
+
+	equal(typeof res.json,'object')
+}
