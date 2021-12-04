667d62035666ae72a237a61aa05cf5bfac23e427
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-12-04 13:35:48 +1100

    Update test

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import request from './src/index'
 
 (async () => {
-	console.log((await request('htts://rt-03.s3.ap-southeast-2.amazonaws.com/animals/2EPZuYEwkU.jpg').send()).buffer)
+	console.log((await request('https://jsonplaceholder.typicode.com/posts').send()).json)
 })()
